b2c50221c012e91985ca81c0a8a1549f37e2e2a4
rafael_lucio <rafaellucio.developer@gmail.com>
2020-07-30 18:02:19 -0300

    variaveis

---
+++ logica-variavel/src/main/java/logica/variavel/Main.java
@@ -0,0 +1,29 @@
+package logica.variavel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//int idade;
+		//idade = 30;
+		
+		int idade = 30;
+		double nota = 9.8;
+		boolean ligado = true;
+		boolean ativo = false;
+		String nome = "Rafael Lucio";
+		
+		System.out.println(idade);
+		System.out.println(nome + " -> " + idade);
+		
+		int numeroDePessoas = 35;
+		
+		var x = 5;
+		var y = "abc";
+		var z = 2.2;
+		var w = true;
+
+		
+	}
+
+}
