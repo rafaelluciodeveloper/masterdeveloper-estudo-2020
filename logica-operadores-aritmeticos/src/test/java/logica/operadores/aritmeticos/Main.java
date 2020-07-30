cbc0c5ffbf3aa750cbc2c3b30ccfa8b8533602c7
rafael_lucio <rafaellucio.developer@gmail.com>
2020-07-30 18:39:23 -0300

    logica operadores aritimeticos

---
+++ logica-operadores-aritmeticos/src/test/java/logica/operadores/aritmeticos/Main.java
@@ -0,0 +1,59 @@
+package logica.operadores.aritmeticos;
+
+import br.com.softblue.commons.io.Console;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*
+		String texto1;
+		String texto2;
+		
+		System.out.println("Digite o texto 1:");
+		texto1 = Console.readString();
+
+		System.out.println("Digite o texto 2:");
+		texto2 = Console.readString();
+		
+		String textoJunto = texto1 + texto2;
+		
+		System.out.println("Texto final: " + textoJunto);
+		*/
+		
+		/*
+		System.out.println("a:");
+		int a = Console.readInt();
+		
+		System.out.println("b:");
+		int b = Console.readInt();
+		
+		System.out.println("b:");
+		int c = Console.readInt();
+		
+		
+		double delta = Math.pow(b, 2) - 4 * a * c;
+		
+		double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+		double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+		System.out.println("x1 = " + x1);
+		System.out.println("x2 = " + x2);
+	
+	*/
+	
+		/*
+		int lado = Console.readInt();
+		int perimetro = 6 * lado;
+		double area = (3 * lado * Math.sqrt(3)) / 2;
+		
+		System.out.println("Perimetro = " + perimetro);
+		System.out.println("Area = " + area);
+		*/
+		
+		int resto = 10 % 2;
+		System.out.println(resto);
+	}
+	
+
+}
