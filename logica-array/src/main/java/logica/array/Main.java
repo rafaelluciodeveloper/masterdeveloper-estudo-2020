40d3073ef9a15512c4e60102961688b9e36cb188
rafael_lucio <rafaellucio.developer@gmail.com>
2020-07-31 19:37:20 -0300

    logica array

---
+++ logica-array/src/main/java/logica/array/Main.java
@@ -0,0 +1,46 @@
+package logica.array;
+
+import br.com.softblue.commons.io.Console;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		System.out.println("Digite a nota 1:");
+		double nota1 = Console.readDouble();
+		
+		System.out.println("Digite a nota 2:");
+		double nota2 = Console.readDouble();
+		
+		System.out.println("Digite a nota 3:");
+		double nota3 = Console.readDouble();
+
+		System.out.println("Notas:");
+		System.out.println(nota1);
+		System.out.println(nota2);
+		System.out.println(nota3);
+	*/
+		
+		/*
+		double[] notas = new double[3];
+		
+		for(int i = 0 ; i < notas.length ; i++) {
+			System.out.println("Digite a nota " + i + 1);
+			notas[i] = Console.readDouble();
+		}
+		
+		for(double nota : notas) {
+			System.out.println(nota);
+		}
+		
+		*/
+		
+		double[] notas2 = {8.5 , 9.3 , 8.6};
+		
+		for(double nota : notas2) {
+			System.out.println(nota);
+		}
+		
+	}
+
+}
